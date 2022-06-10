a0b1565aec0d993c9587ffce03de048ac015c47b
Platformer444 <78246772+Platformer444@users.noreply.github.com>
2022-06-10 15:36:03 +0530

    Updated to version 1.1

--- py_tools.py
+++ py_tools.py
@@ -1,3 +1,9 @@
+import datetime as dt
+
+print("""Hello from py_tools! 
+If you like py_tools then make sure to star it. 
+If you find any issues then report it at https://github.com/Platformer444/py_tools/isses .""")
+
 # Doing some arithmetic operation with the nmbers in the specified list
 def Arithmetic_Operation_With_Lists(listToBeCalculated, Operation):
 	num = 0
@@ -29,7 +35,22 @@ def Join_Everything_In_List(listToBeJoined):
 	print(Result)
 
 # Get the index of a item in a list
-def Get_Index_Of_A_Character_In_List(listSpecified, Item):
+def Get_Index_Of_Item_In_List(listSpecified, Item):
 	for i in range(len(listSpecified)):
 		if str(listSpecified[int(i)]) == str(Item):
 			print(i)
+
+# Finds the average/mean of numbers
+def Average_Of(numbers):
+	num = 0
+	for i in range(len(numbers)):
+		num = num + int(numbers[int(i)])
+	print(num/len(numbers))
+
+# Print the current date and time
+def Print_Date_And_Time():
+	d_t = dt.datetime.now()
+	Date = d_t.strftime('%d') + '-' + d_t.strftime('%m') + '-' + d_t.strftime('%Y')
+	print(Date)
+	Time = d_t.strftime('%I') + ':' + d_t.strftime('%M') + ':' + d_t.strftime('%S') + ' ' + d_t.strftime('%p')
+	print(Time)
